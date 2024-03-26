7245520be350856703e9e664513a7652cb65d630
johnuwe98 <157540985+johnuwe98@users.noreply.github.com>
2024-03-26 15:50:11 +0100

    Add files via upload

---
+++ jbn.html/john.js
@@ -0,0 +1,4 @@
+alert('Hello John Uwe');
+document.querySelector('h2').style.color = "darkblue"
+document.querySelector('footer').style.color = "red"
+document.querySelector('html').style.color = "blue"
